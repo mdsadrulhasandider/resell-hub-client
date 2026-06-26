bbf7cf73c8e333f94ca0f843d73b2dfd5378f18b
Md Sadrul Hasan Dider <mdsadrulhasandider@gmail.com>
2026-06-27 02:37:14 +0600

    fix: remove duplicate client-side users api endpoint to let Vercel rewrite proxy to server

--- src/app/api/users/[id]/route.js
+++
@@ -1,7 +0,0 @@
-import { updateUser, deleteUser } from "@/controllers/userController.js";
-import { verifyJWT } from "@/middleware/verifyJWT.js";
-import { verifyAdmin } from "@/middleware/verifyAdmin.js";
-import { makeRouteHandler } from "@/lib/api-adapter.js";
-
-export const PATCH = makeRouteHandler(updateUser, [verifyJWT, verifyAdmin]);
-export const DELETE = makeRouteHandler(deleteUser, [verifyJWT, verifyAdmin]);
--- src/app/api/users/route.js
+++
@@ -1,6 +0,0 @@
-import { getAllUsers } from "@/controllers/userController.js";
-import { verifyJWT } from "@/middleware/verifyJWT.js";
-import { verifyAdmin } from "@/middleware/verifyAdmin.js";
-import { makeRouteHandler } from "@/lib/api-adapter.js";
-
-export const GET = makeRouteHandler(getAllUsers, [verifyJWT, verifyAdmin]);

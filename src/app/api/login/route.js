387e650989888f9a86b85aaade8a575605ec9343
Md Sadrul Hasan Dider <mdsadrulhasandider@gmail.com>
2026-06-27 02:52:17 +0600

    fix: remove remaining local api routes to rely fully on proxying

--- src/app/api/login/route.js
+++
@@ -1,4 +0,0 @@
-import { login } from "@/controllers/authController.js";
-import { makeRouteHandler } from "@/lib/api-adapter.js";
-
-export const POST = makeRouteHandler(login);
--- src/app/api/register/route.js
+++
@@ -1,4 +0,0 @@
-import { register } from "@/controllers/authController.js";
-import { makeRouteHandler } from "@/lib/api-adapter.js";
-
-export const POST = makeRouteHandler(register);
